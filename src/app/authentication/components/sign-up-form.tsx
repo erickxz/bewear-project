5bbda064fcb016d9c0803b8541ea4ecc372d5156
erickxz <epcfalcon009@gmail.com>
2025-08-21 20:35:15 -0300

    feat: add sign up

--- src/app/authentication/components/sign-up-form.tsx
+++ src/app/authentication/components/sign-up-form.tsx
@@ -1,13 +1,16 @@
 "use client"
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 
 const formSchema = z
   .object({
@@ -27,6 +30,7 @@ const formSchema = z
 type FormSchema = z.infer<typeof formSchema>;
 
 const SignUpForm = () =>  {
+  const router = useRouter();
     // 1. Define your form.
     const form = useForm<FormSchema>({
       resolver: zodResolver(formSchema),
@@ -39,10 +43,29 @@ const SignUpForm = () =>  {
     })
 
      // 2. Define a submit handler.
-  function onSubmit(values: FormSchema) {
-    console.log("Formulário Válido e Enviado");
-    console.log(values)
+  async function onSubmit(values: FormSchema) {
+
+    await authClient.signUp.email({
+        name: values.name, // required
+        email: values.email, // required
+        password: values.password, // require
+        fetchOptions: {
+            onSuccess: () => {
+                router.push("/")
+            },
+            onError: (error) => {
+                
+               if (error.error.code === "USER_ALREADY_EXISTS"
+               ) {
+                    toast.error("Email já em uso")
+                    form.setError("email", { message: "Email já cadastrado" })
+                } 
+                toast.error(error.error.message)
+            }
+        }
+    })
   }
+
 
     return (
         <>        
