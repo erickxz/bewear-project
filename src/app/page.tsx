3598fd20ba2066146348ec3de48b54c4cca44461
erickxz <epcfalcon009@gmail.com>
2025-08-23 08:27:39 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import ProductsList from "@/components/common/products-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 const Home = async () => {
+  
+
   const products = await db.query.productTable.findMany({with: {variants: true}});
+  const newCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with: {variants: true}
+  });
   const categories = await db.query.categoryTable.findMany();
   console.log(products);
   return (
@@ -27,7 +36,11 @@ const Home = async () => {
       <div className="px-5">
       <Image src="/banner.png" alt="Seja autentico" height={0} width={0} sizes="100vw" className="h-auto w-full" />
       </div>
+
+      <ProductsList title="Novos produtos" products={newCreatedProducts} />
+      <Footer />
     </div>
+    
     </>
   );
 }
