a421f8e0c5becb472aad19cc42270dd6b9588d97
erickxz <epcfalcon009@gmail.com>
2025-08-27 19:47:24 -0300

    feat: category page

--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { categoryTable } from "@/db/schema";
 
 import { Button } from "../ui/button";
@@ -11,7 +13,8 @@ const CategorySelector = ({ categories }: CategorySelectorProps) => {
 <div className="grid grid-cols-2 gap-3">
 {categories.map((category) => (
     <Button key={category.id} variant="ghost" className="bg-white rounded-full font-semibold text-xs">
-        {category.name}
+        <Link href={`/category/${category.slug}`}>{category.name}
+        </Link>
     </Button>
 ))}
 </div>
