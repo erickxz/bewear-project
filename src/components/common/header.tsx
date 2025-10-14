dc92851506cc39d8679d4602cae70d24333aedb7
erickxz <epcfalcon009@gmail.com>
2025-10-14 20:53:00 -0300

    style: header separator

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -8,6 +8,7 @@ import { authClient } from "@/lib/auth-client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
 import {
   Sheet,
   SheetContent,
@@ -19,17 +20,27 @@ import { Cart } from "./cart";
 
 export const Header = () => {
   const { data: session } = authClient.useSession();
+
   return (
     <header className="flex items-center justify-between p-5">
       <Link href="/">
         <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
       </Link>
 
-      <div className="flex items-center gap-3">
-      <Cart />
+      <div className="flex items-center gap-3 h-6">
+  
+        <div className="relative flex items-center">
+          <Cart />
+        </div>
+
+        <Separator
+          orientation="vertical"
+          className="h-1 w-px shrink-0 bg-border"
+        />
+
         <Sheet>
           <SheetTrigger asChild>
-            <Button variant="outline" size="icon">
+            <Button variant="ghost" size="icon">
               <MenuIcon />
             </Button>
           </SheetTrigger>
@@ -39,39 +50,37 @@ export const Header = () => {
             </SheetHeader>
             <div className="px-5">
               {session?.user ? (
-                <>
-                  <div className="flex justify-between space-y-6">
-                    <div className="flex items-center gap-3">
-                      <Avatar>
-                        <AvatarImage
-                          src={session?.user?.image as string | undefined}
-                        />
-                        <AvatarFallback>
-                          {session?.user?.name?.split(" ")?.[0]?.[0]}
-                          {session?.user?.name?.split(" ")?.[1]?.[0]}
-                        </AvatarFallback>
-                      </Avatar>
+                <div className="flex justify-between space-y-6">
+                  <div className="flex items-center gap-3">
+                    <Avatar>
+                      <AvatarImage
+                        src={session?.user?.image as string | undefined}
+                      />
+                      <AvatarFallback>
+                        {session?.user?.name?.split(" ")?.[0]?.[0]}
+                        {session?.user?.name?.split(" ")?.[1]?.[0]}
+                      </AvatarFallback>
+                    </Avatar>
 
-                      <div>
-                        <h3 className="font-semibold">{session?.user?.name}</h3>
-                        <span className="text-muted-foreground block text-xs">
-                          {session?.user?.email}
-                        </span>
-                      </div>
+                    <div>
+                      <h3 className="font-semibold">{session?.user?.name}</h3>
+                      <span className="text-muted-foreground block text-xs">
+                        {session?.user?.email}
+                      </span>
                     </div>
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      onClick={() => authClient.signOut()}
-                    >
-                      <LogOutIcon />
-                    </Button>
                   </div>
-                </>
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    onClick={() => authClient.signOut()}
+                  >
+                    <LogOutIcon />
+                  </Button>
+                </div>
               ) : (
                 <div className="flex items-center justify-between">
                   <h2 className="font-semibold">Olá. Faça seu login!</h2>
-                  <Button size="icon" asChild variant="outline">
+                  <Button size="icon" asChild variant="ghost">
                     <Link href="/authentication">
                       <LogInIcon />
                     </Link>
@@ -81,7 +90,6 @@ export const Header = () => {
             </div>
           </SheetContent>
         </Sheet>
-        
       </div>
     </header>
   );
